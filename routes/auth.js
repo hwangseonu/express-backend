b440b2370952fe3eea06fea05aa1d083ac42c402
hwangseonu <hwangseonu12@naver.com>
2018-12-03 23:30:19 +0900

    feat: Add refresh token route

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const {secret, access_exp, refresh_exp} = require('../config').jwt;
 const json_required = require('../middlewares/json_required');
+const auth_required = require('../middlewares/auth_required');
 
 const router = express.Router();
 
-router.post("/", json_required({username: 'string', password: 'string'}));
-router.post("/", async function (req, res, next) {
+router.post('/', json_required({username: 'string', password: 'string'}));
+router.post('/', async function (req, res, next) {
   const {username, password} = req.body;
   try {
     const user = await User.findOne({username: username, password: password});
@@ -28,6 +29,20 @@ router.post("/", async function (req, res, next) {
   } catch (error) {
     res.status(401).json({message: error.message});
   }
+});
+
+router.get('/', auth_required('refresh'));
+router.get('/', async function (req, res, next) {
+  const access = await generateToken(req.user.username, 'access');
+  const refresh = await generateToken(req.user.username, 'refresh');
+
+  const result = {access: access};
+
+  if ((req.token_exp - (new Date().getTime() / 1000)) < 604800) {
+    result.refresh = refresh;
+  }
+
+  res.json(result)
 });
 
 async function generateToken(username, type) {
